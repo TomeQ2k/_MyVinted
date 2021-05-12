f806bdc567bf3c8e856931e83f2df8c104611aae
TomeQ2k <48593674+TomeQ2k@users.noreply.github.com>
2021-05-13 01:49:52 +0200

    Update ISpecification.cs

--- MyVinted.Core.Application/Specifications/ISpecification.cs
+++ MyVinted.Core.Application/Specifications/ISpecification.cs
@@ -6,6 +6,7 @@ namespace MyVinted.Core.Application.Specifications
     public interface ISpecification<T> where T : class, new()
     {
         bool IsSatisfied(T model);
+        
         Expression<Func<T, bool>> ToExpression();
     }
 }
