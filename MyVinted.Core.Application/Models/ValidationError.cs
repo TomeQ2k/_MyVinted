229e5fb7628f4d3ad239efe90bf0e3389d818cae
TomeQ2k <48593674+TomeQ2k@users.noreply.github.com>
2021-06-09 13:06:33 +0200

    Update ValidationError.cs

--- MyVinted.Core.Application/Models/ValidationError.cs
+++ MyVinted.Core.Application/Models/ValidationError.cs
@@ -5,9 +5,9 @@ namespace MyVinted.Core.Application.Models
     public record ValidationError
     {
         public string Field { get; }
-        public IEnumerable<string> Message { get; }
+        public IEnumerable<string> Messages { get; }
 
         public ValidationError(string field, IEnumerable<string> messages)
-            => (Field, Message) = (field != string.Empty ? field : null, messages);
+            => (Field, Messages) = (field != string.Empty ? field : null, messages);
     }
 }
